811648abb7922b075d183b5ebd61d07baae34dfb
Ricardo Ojeda <ricardo270298@gmail.com>
2022-01-27 17:50:18 -0500

    Agregada parte de la data de Savannah, Agregada función buscarRepetidos

--- analisis-data-directo/analisis.js
+++ analisis-data-directo/analisis.js
@@ -1,13 +1,74 @@
-let busqueda = [];
+const savannah = candidatos.filter(function(candidato){
+    return candidato.afiliadora == "savannah";
+});
+const leycy = candidatos.filter(function(candidato){
+    return candidato.afiliadora == "leycy";
+});
 
-function filtrarFase1(lista){
-    busqueda = [];
-    
-    for (let i = 0; i < lista.length; i++){
-        if(busqueda[lista[i].fase1]) {
-            busqueda[lista[i].fase1] += 1;
-        }else {
-            busqueda[lista[i].fase1] = 1;
+function contactados(lista) {
+    return lista.filter(
+        function(candidato){
+            return candidato.fase1 != "noCont";
         }
-    }
+    );
+}
+
+// let busqueda = [];
+
+// function filtrarFase1(lista){
+//     busqueda = [];
+    
+//     for (let i = 0; i < lista.length; i++){
+//         if(busqueda[lista[i].fase1]) {
+//             busqueda[lista[i].fase1] += 1;
+//         }else {
+//             busqueda[lista[i].fase1] = 1;
+//         }
+//     }
+// }
+
+function buscarRepetidos(lista) {
+    const listaCount = {};
+
+    lista.map(
+        function(candidato) {
+            if(listaCount[candidato.dni]) {
+                listaCount[candidato.dni] += 1;
+            }
+            else {
+                listaCount[candidato.dni] = 1;
+            }
+        }
+    );
+
+    const listaArray = Object.entries(listaCount).filter(
+        function (item) {
+            return item[1] > 1;
+        }
+    );
+
+    listaArray.map(
+        function(elemento) {
+            console.log(elemento[0]
+                + " aparece "
+                + elemento[1]
+                + " veces");
+        }
+    );
+    
+    const repetidos = [];
+
+    listaArray.forEach(
+        function (dni) {
+            let dniNumber = dni[0];
+            repetidos.push(lista.filter(
+                    function(candidato){
+                        return candidato.dni === dniNumber;
+                    }
+                )
+            );
+        }
+    );
+
+    return repetidos;
 }
